1e5bb5812e999f9ee4f454d6c2ad540119387a35
douglascdev <douglasc.dev@gmail.com>
2024-05-18 14:24:18 -0300

    Prepare Emote and ChatUser models

--- hasherino/model.go
+++ hasherino/model.go
@@ -1,7 +1,12 @@
 package hasherino
 
-import "gorm.io/gorm"
+import (
+	"errors"
 
+	"gorm.io/gorm"
+)
+
+// --- permDB models ---
 type Account struct {
 	Id          string `gorm:"primaryKey"`
 	Login       string
@@ -22,4 +27,38 @@ type AppSettings struct {
 	gorm.Model
 	ChatMessageLimit int // Maximum amount of messages in a single chat
 	ChatHistory      bool
+}
+
+// --- tempDB models ---
+type EmoteSourceEnum int64
+
+const (
+	Twitch EmoteSourceEnum = iota
+	SevenTV
+)
+
+type ChatUser struct {
+	Id          string `gorm:"primaryKey"`
+	Login       string
+	DisplayName string
+	Emotes      []Emote `gorm:"constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
+}
+
+type Emote struct {
+	Id        string          `gorm:"primaryKey"`
+	Source    EmoteSourceEnum `gorm:"primaryKey"`
+	Name      string
+	ChannelID *string   // if a channel is set, only renders in that channel
+	Owner     *ChatUser // if an owner is set, only renders when the message sender is the owner
+}
+
+func (e *Emote) GetUrl() (string, error) {
+	switch e.Source {
+	case Twitch:
+		return "https://static-cdn.jtvnw.net/emoticons/v2/" + e.Id + "/default/dark/2.0", nil
+	case SevenTV:
+		return "https://cdn.7tv.app/emote/" + e.Id + "/2x.webp", nil
+	default:
+		return "", errors.New("Unknown emote source")
+	}
 }
